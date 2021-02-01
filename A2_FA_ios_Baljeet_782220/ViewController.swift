ebd50a4dde4e06515b5541b52ecad1a17582ea82
Baljeet <baljeetamrit786@gmail.com>
2021-02-01 02:57:41 -0500

    Initial Commit

---
+++ A2_FA_ios_Baljeet_782220/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_FA_ios_Baljeet_782220
+//
+//  Created by Mac on 01/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
